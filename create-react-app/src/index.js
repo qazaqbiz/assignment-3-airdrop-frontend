f5ede42e5d09cab61cbbf3e5300f4219bd030893
Ilya Mikheev <mikheev.show@gmail.com>
2023-03-28 17:13:27 +0300

    Update index.js

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { ethers } from 'ethers';
+
+async function connectWeb3() {
+  if (window.ethereum) {
+    try {
+      await window.ethereum.enable();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      console.log('Connected to Web3:', await signer.getAddress());
+    } catch (error) {
+      console.error('Failed to connect to Web3:', error);
+    }
+  } else {
+    console.error('No Web3 provider found');
+  }
+}
+
+connectWeb3();
 
 ReactDOM.render(
   <React.StrictMode>
