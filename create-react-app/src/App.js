07de00145f106c70ca384bc1e8898448339f37c0
Ilya Mikheev <mikheev.show@gmail.com>
2023-03-28 17:13:08 +0300

    Update App.js

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; // Импортируем useState для хранения состояния компонента
+import { claimAirdrop } from './Airdrop';
 
 function App() {
+  const [address, setAddress] = useState('');
+
+  async function handleClaim() {
+    const result = await claimAirdrop(address);
+    alert(result);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter your Ethereum address to claim the airdrop:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={address} onChange={event => setAddress(event.target.value)} />
+        <button onClick={handleClaim}>Claim Airdrop</button>
       </header>
     </div>
   );
